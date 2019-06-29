1a4e57758a1b3b3c2bcd00e05f0952b6f9f91204
evanmacbride <ejmacbride@gmail.com>
2019-06-28 21:18:55 -0400

    transfer src files

--- src/App.js
+++ src/App.js
@@ -1,50 +1,169 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React from "react"
+//import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+// Find the asset that includes the query name in its title. If no
+// asset contains the query name, return the first one in the array.
+/*function findMainAsset(assetTitleArray, query) {
+	for (let i = 0; i < assetTitleArray.length; i++) {
+		if (assetTitleArray[i].includes(query)) {
+			return assetTitleArray[i];
+		}
+	}
+	return assetTitleArray[0];
+}*/
 
-  handleClick = api => e => {
-    e.preventDefault()
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			pageText: [],
+			query: 'Philadelphia',
+			/*imageUrl: '',
+			imageTitle: ''*/
+		};
+	}
+	
+	// Fetch the page text as extracts with the prop=extracts parameter. 
+	// Use explaintext= to remove HTML tags. split() separates the text
+	// into an array by returns and newlines.
+	//
+	// TODO: Remove empty '()' that contained removed pronunciation 
+	// guides. Make sure periods don't elide into next sentence.
+	//
+	componentDidMount() {
+		fetch("https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=extracts&titles=" + 
+			this.state.query + "&format=json&explaintext=")
+			.then(response => response.json())
+			.then(response => {
+				console.log(Object.values(response.query.pages)[0].extract);
+				// Process the text.
+				
+				// Use booleans to apply appropriate classes to set margins
+				// to keep the baseline grid before and after h 
+				// tags maintained.
+				let beneathH3 = false;
+				let beneathP = false;
+				
+				// Use lastTag and deleteList to detect identical adjacent h
+				// tags and flag them for removal. All but the last of a set
+				// of identical h tags would be a header for an empty section.
+				let lastTag = '';
+				let deleteList = [];
+				
+				// Use breaklist to catch header text that indicates the main
+				// text has ended.
+				const breakList = ["== GALLERY ==", "== NOTES ==", 
+					"== REFERENCES ==",	"== EXTERNAL LINKS ==", "== SEE ALSO =="];
+				const breakPoints = [];
+				
+				let pArray = Object.values(response.query.pages)[0].extract.split(/[\r\n]+/)
+					.map((text, index) => {
+						
+						// Ignore text that's all whitespace.
+						text = text.trim();
+						if (breakList.indexOf(text.toUpperCase()) > -1) {
+							breakPoints.push(index);
+						}
+						if (text === "") {
+							return null;
+						}
+						else if (text.includes("====")) {
+							
+							// Catch identical adjacent h tags
+							if (lastTag === "h4") {
+								deleteList.push(index - 1);
+							}
+							lastTag = "h4";
+							
+							// Remove old formatting and apply new h tag
+							const find = "====";
+							const re = new RegExp(find, 'g');
+							text = text.replace(re, '').trim();
+							
+							// Apply appropriate class for proper 
+							// baseline alignment
+							beneathH3 = false;
+							if (beneathP) {
+								beneathP = false;
+								return <h4 className="beneathP" key={index}>{text}</h4>;
+							}
+							return <h4 key={index}>{text}</h4>;
+						}
+						else if (text.includes("===")) {
+							if (lastTag === "h3") {
+								deleteList.push(index - 1);
+							}
+							lastTag = "h3";
+							const find = "===";
+							const re = new RegExp(find, 'g');
+							text = text.replace(re, '').trim();
+							beneathH3 = true;
+							beneathP = false;
+							return <h3 key={index}>{text}</h3>;						
+						}
+						else if (text.includes("==")) {
+							if (lastTag === "h2") {
+								deleteList.push(index - 1);
+							}
+							lastTag = "h2";
+							const find = "==";
+							const re = new RegExp(find, 'g');
+							text = text.replace(re, '').trim();
+							return <h2 key={index}>{text}</h2>;						
+						}
+						lastTag = "p";
+						beneathP = true;
+						if (beneathH3) {
+							beneathH3 = false;
+							return <p className="beneathH3" key={index}>{text}</p>;
+						}
+						return <p key={index}>{text}</p>;
+					});
+					
+				// Remove text after first breakpoint
+				pArray = pArray.slice(0, breakPoints[0]);
+				
+				// Go back and remove headers for empty sections.
+				for (let i = 0; i < deleteList.length; i++) {
+					pArray.splice(deleteList[i], 1);
+				}
+				this.setState({
+					pageText: pArray
+				})
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
+			});
+		// Fetch all image titles.
+		/*fetch("https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=images&titles=" + 
+			this.state.query + "&format=json")
+			.then(response => response.json())
+			.then(response => {
+				const imgTitleArray = Object.values(response.query.pages)[0].images
+					.map(item => item.title);
+				const mainImgTitle = findMainAsset(imgTitleArray, this.state.query);
+				return mainImgTitle;
+				})
+			.then(title =>  {
+				fetch("https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=" + 
+				title + "&prop=imageinfo&iiprop=url&format=json")
+				.then(response => response.json())
+				.then(response => {
+					const url = Object.values(response.query.pages)[0].imageinfo[0].url;
+					this.setState({imgUrl: url, imageTitle: title})
+					})
+				}
+			)*/
+	}
+	
+	render() {
+		return (
+			<main>
+				<article className="pageText">
+					<h1>{this.state.query}</h1>
+					{this.state.pageText}
+				</article>
+			</main>
+		)
+	}
 }
 
 export default App
