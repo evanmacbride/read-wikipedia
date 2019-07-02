8a3be4a65aa7974697ec5277481399c1d1d5e038
evanmacbride <ejmacbride@gmail.com>
2019-07-01 23:10:05 -0400

    add functional search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
-//import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import Page from "./Page"
+import Header from "./Header"
 
 // Find the asset that includes the query name in its title. If no
 // asset contains the query name, return the first one in the array.
@@ -16,28 +17,43 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			pageText: [],
-			query: 'Philadelphia',
-			/*imageUrl: '',
-			imageTitle: ''*/
+			pageText: null,
+			query: null,
+			title: null
 		};
+		this.handleFormChange = this.handleFormChange.bind(this);
+		this.handleFormSubmit = this.handleFormSubmit.bind(this);
+		
 	}
 	
-	// Fetch the page text as extracts with the prop=extracts parameter. 
-	// Use explaintext= to remove HTML tags. split() separates the text
-	// into an array by returns and newlines.
-	//
-	// TODO: Remove empty '()' that contained removed pronunciation 
-	// guides. Make sure periods don't elide into next sentence.
-	//
-	componentDidMount() {
+	/*handleFormSubmit(event) {
+		event.preventDefault();
+		this.setState({
+			query: event.target.value
+		});
+	}*/
+	
+	handleFormChange(text) {
+		this.setState({
+			query: text
+		});		
+	}
+	
+	handleFormSubmit() {
+		//this.setState({query: query});
+		console.log(this.state.query);
 		fetch("https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=extracts&titles=" + 
-			this.state.query + "&format=json&explaintext=")
+			this.state.query + "&format=json&redirects=&explaintext=")
 			.then(response => response.json())
 			.then(response => {
+				// Get the title
+				this.setState({title: Object.values(response.query.pages)[0].title});
+				// Process the text.
+				
+				// Restore newlines lost by ignoring footnotes.
 				let responseText = Object.values(response.query.pages)[0].extract;
 				responseText = responseText.replace(/([a-z0-9])\.([A-Z])/g,'$1\n$2');
-				// Process the text.
+				
 				
 				// Use booleans to apply appropriate classes to set margins
 				// to keep the baseline grid before and after h 
@@ -58,67 +74,67 @@ class App extends React.Component {
 				const breakPoints = [];
 				
 				let textArray = responseText.split(/[\r\n]+/).map((text, index) => {
+					
+					// Ignore text that's all whitespace.
+					text = text.trim();
+					if (breakList.indexOf(text.toUpperCase()) > -1) {
+						breakPoints.push(index);
+					}
+					if (text === "") {
+						return null;
+					}
+					else if (text.includes("====")) {
 						
-						// Ignore text that's all whitespace.
-						text = text.trim();
-						if (breakList.indexOf(text.toUpperCase()) > -1) {
-							breakPoints.push(index);
+						// Catch identical adjacent h tags
+						if (lastTag === "h4") {
+							deleteList.push(index - 1);
 						}
-						if (text === "") {
-							return null;
-						}
-						else if (text.includes("====")) {
-							
-							// Catch identical adjacent h tags
-							if (lastTag === "h4") {
-								deleteList.push(index - 1);
-							}
-							lastTag = "h4";
-							
-							// Remove old formatting and apply new h tag
-							const find = "====";
-							const re = new RegExp(find, 'g');
-							text = text.replace(re, '').trim();
-							
-							// Apply appropriate class for proper 
-							// baseline alignment
-							beneathH3 = false;
-							if (beneathP) {
-								beneathP = false;
-								return <h4 className="beneathP" key={index}>{text}</h4>;
-							}
-							return <h4 key={index}>{text}</h4>;
-						}
-						else if (text.includes("===")) {
-							if (lastTag === "h3") {
-								deleteList.push(index - 1);
-							}
-							lastTag = "h3";
-							const find = "===";
-							const re = new RegExp(find, 'g');
-							text = text.replace(re, '').trim();
-							beneathH3 = true;
+						lastTag = "h4";
+						
+						// Remove old formatting and apply new h tag
+						const find = "====";
+						const re = new RegExp(find, 'g');
+						text = text.replace(re, '').trim();
+						
+						// Apply appropriate class for proper 
+						// baseline alignment
+						beneathH3 = false;
+						if (beneathP) {
 							beneathP = false;
-							return <h3 key={index}>{text}</h3>;						
+							return <h4 className="beneathP" key={index}>{text}</h4>;
 						}
-						else if (text.includes("==")) {
-							if (lastTag === "h2") {
-								deleteList.push(index - 1);
-							}
-							lastTag = "h2";
-							const find = "==";
-							const re = new RegExp(find, 'g');
-							text = text.replace(re, '').trim();
-							return <h2 key={index}>{text}</h2>;						
+						return <h4 key={index}>{text}</h4>;
+					}
+					else if (text.includes("===")) {
+						if (lastTag === "h3") {
+							deleteList.push(index - 1);
 						}
-						lastTag = "p";
-						beneathP = true;
-						if (beneathH3) {
-							beneathH3 = false;
-							return <p className="beneathH3" key={index}>{text}</p>;
+						lastTag = "h3";
+						const find = "===";
+						const re = new RegExp(find, 'g');
+						text = text.replace(re, '').trim();
+						beneathH3 = true;
+						beneathP = false;
+						return <h3 key={index}>{text}</h3>;						
+					}
+					else if (text.includes("==")) {
+						if (lastTag === "h2") {
+							deleteList.push(index - 1);
 						}
-						return <p key={index}>{text}</p>;
-					});
+						lastTag = "h2";
+						const find = "==";
+						const re = new RegExp(find, 'g');
+						text = text.replace(re, '').trim();
+						return <h2 key={index}>{text}</h2>;						
+					}
+					lastTag = "p";
+					beneathP = true;
+					if (beneathH3) {
+						beneathH3 = false;
+						return <p className="beneathH3" key={index}>{text}</p>;
+					}
+					return <p key={index}>{text}</p>;
+				});
 					
 				// Remove text after first breakpoint
 				textArray = textArray.slice(0, breakPoints[0]);
@@ -130,38 +146,23 @@ class App extends React.Component {
 				this.setState({
 					pageText: textArray
 				})
-
-			});
-		// Fetch all image titles.
-		/*fetch("https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=images&titles=" + 
-			this.state.query + "&format=json")
-			.then(response => response.json())
-			.then(response => {
-				const imgTitleArray = Object.values(response.query.pages)[0].images
-					.map(item => item.title);
-				const mainImgTitle = findMainAsset(imgTitleArray, this.state.query);
-				return mainImgTitle;
-				})
-			.then(title =>  {
-				fetch("https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=" + 
-				title + "&prop=imageinfo&iiprop=url&format=json")
-				.then(response => response.json())
-				.then(response => {
-					const url = Object.values(response.query.pages)[0].imageinfo[0].url;
-					this.setState({imgUrl: url, imageTitle: title})
-					})
-				}
-			)*/
+				console.log(this.state.pageText);
+			}
+		);
 	}
 	
 	render() {
 		return (
-			<main>
-				<article className="pageText">
-					<h1>{this.state.query}</h1>
-					{this.state.pageText}
-				</article>
-			</main>
+			<div>
+				<Header 
+					onFormChange={this.handleFormChange}
+					onFormSubmit={this.handleFormSubmit}
+				/>
+				<Page 
+					title={this.state.title}
+					pageText={this.state.pageText} 
+				/>
+			</div>
 		)
 	}
 }
