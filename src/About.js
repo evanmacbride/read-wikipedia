43af14f36831053790ec9c4327e4fb1dff3273e6
evanmacbride <ejmacbride@gmail.com>
2019-09-10 08:59:11 -0400

    update about page text

--- src/About.js
+++ src/About.js
@@ -7,12 +7,14 @@ const About = () => (
 				<h1>About</h1>
 			</header>
 			<p>
-				Read Wikipedia is a project by Evan MacBride. It was built with	<a href="https://reactjs.org/">React</a> and the <a href="https://www.mediawiki.org/wiki/API:Main_page">Wikipedia API</a>. 
+				Read Wikipedia is a project by Evan MacBride. It was built with	<a href="https://reactjs.org/">React</a> and the <a href="https://www.mediawiki.org/wiki/API:Main_page">Wikipedia API</a>. It is not affiliated with Wikipedia or the Wikimedia Foundation in any way.
+      </p>
+      <p> 
 				You can see other projects Evan has made on his <a href="https://evanmacbride.dev">personal website</a>,
 				and you can view the GitHub repository for Read Wikipedia <a href="https://github.com/evanmacbride/read-wikipedia">here</a>.
 			</p>
 			<p>
-				If you enjoyed this project, or if you enjoy Wikipedia in 
+				If you enjoyed this project, or if you enjoy Wikipedia in
 				general, please consider <a href="https://wikimediafoundation.org/">donating</a> to the Wikimedia Foundation directly.
 			</p>
 		</article>
